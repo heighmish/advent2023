9bbacf7e587cbc51ce015f82f543875c2c1af2e9
Hamish Prosser <59728957+heighmish@users.noreply.github.com>
2024-01-07 17:37:42 +1100

    day9 (#3)

---
+++ day9/Part2.cs
@@ -0,0 +1,52 @@
+
+public class Part2
+{
+    public int Run(string line)
+    {
+        var initalPatternList = line.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToList();
+
+        var lists = new List<List<int>>
+        {
+            initalPatternList
+        };
+
+        while (!lists.Last().All(x => x == 0))
+        {
+            lists.Add(GetDifferenceList(lists.Last()));
+        }
+        foreach (var list in lists)
+        {
+            foreach (var num in list)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+        }
+        var sum = 0;
+
+        var lowerRank = 0;
+        for (var outer = lists.Count - 1; outer >= 0; outer--)
+        {
+            // Console.WriteLine($"Outer is: {outer}");
+            var lastElement = lists[outer].First();
+            sum = lastElement - lowerRank;
+            lowerRank = sum;
+            Console.WriteLine($"Extrapolated value {sum}");
+        }
+        return sum;
+    }
+
+    public static List<int> GetDifferenceList(List<int> list)
+    {
+        var ret = new List<int>();
+        var curr = list.First();
+        foreach (var num in list.Skip(1))
+        {
+            ret.Add(num - curr);
+            curr = num;
+        }
+        return ret;
+    }
+}
---
+++ day9/Program.cs
@@ -0,0 +1,14 @@
+﻿var lines = File.ReadLines("big.txt");
+
+var sum = 0;
+var sum2 = 0;
+foreach (var line in lines)
+{
+    var history = new Part1().Run(line);
+    sum += history;
+    sum2 += new Part2().Run(line);
+    Console.WriteLine();
+}
+
+Console.WriteLine($"{sum}");
+Console.WriteLine($"{sum2}");
